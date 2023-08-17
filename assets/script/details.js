9649fee50ee4042f8ebba746abdd73badc898a33
Naza_Ramos <nazareno.94@hotmail.com>
2023-08-17 13:49:16 -0300

    correccion functions.js removiendo funciones que no se repetian

--- assets/script/details.js
+++ assets/script/details.js
@@ -1,4 +1,3 @@
-import { crearTarjeta, renderizarTarjeta } from "../script/module/functions.js";
 let contenedorDetails = document.getElementById("detailsContenedor")
 let parametroEvento = location.search
 let objetoURLSearchParam = new URLSearchParams(parametroEvento)
@@ -14,3 +13,25 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")
     let estructuraHTML = crearTarjeta(eventoID)
     renderizarTarjeta(contenedorDetails, estructuraHTML)
 })
+
+function crearTarjeta(objeto){
+    return `<section class="card" style="width: 18rem;">
+                <img src="${objeto.image}" class="card-img-top" alt="school_book_fair">
+                <div class="card-body d-flex flex-column align-items-center text-center">
+                    <h5 class="card-title">${objeto.name}</h5>
+                    <p class="card-text">${objeto.description}</p>
+                    <p>${objeto.category}</p>
+                </div>
+                <div class="card-body d-flex flex-column justify-content-around align-items-center">
+                    <p>Price: $${objeto.price}</p>
+                    <p>Date: ${objeto.date}</p>
+                    <p>Location: ${objeto.place}</p>
+                    <p>${objeto.assistance?(`Assistance: ${objeto.assistance}`):(`Estimate: ${objeto.estimate}`)}</p>
+                    <p>Capacity: ${objeto.capacity}</p>
+                </div>
+            </section>`
+}
+
+function renderizarTarjeta(contenedorHTML, string){
+    contenedorHTML.innerHTML = string
+}
--- assets/script/module/functions.js
+++ assets/script/module/functions.js
@@ -59,26 +59,3 @@ export function mostrarTarjetaPastUp(evento) {
          </section>`;
   }
 
-//FUNCIONES DETAILS//
-
-export function crearTarjeta(objeto){
-    return `<section class="card" style="width: 18rem;">
-                <img src="${objeto.image}" class="card-img-top" alt="school_book_fair">
-                <div class="card-body d-flex flex-column align-items-center text-center">
-                    <h5 class="card-title">${objeto.name}</h5>
-                    <p class="card-text">${objeto.description}</p>
-                    <p>${objeto.category}</p>
-                </div>
-                <div class="card-body d-flex flex-column justify-content-around align-items-center">
-                    <p>Price: $${objeto.price}</p>
-                    <p>Date: ${objeto.date}</p>
-                    <p>Location: ${objeto.place}</p>
-                    <p>${objeto.assistance?(`Assistance: ${objeto.assistance}`):(`Estimate: ${objeto.estimate}`)}</p>
-                    <p>Capacity: ${objeto.capacity}</p>
-                </div>
-            </section>`
-}
-
-export function renderizarTarjeta(contenedorHTML, string){
-    contenedorHTML.innerHTML = string
-}
